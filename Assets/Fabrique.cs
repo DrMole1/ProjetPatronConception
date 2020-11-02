9c55e5e637f0e3d43958ca2ecf7c92490c839628
DrMole1 <32479979+DrMole1@users.noreply.github.com>
2020-11-02 13:52:34 +0100

    Projet Patron Conception

---
+++ Assets/Fabrique.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fabrique : MonoBehaviour
+{
+    // ========================== VARIABLES ==========================
+
+    [Header("Objects in the scene")]
+    [SerializeField]
+    private Transform spawner;
+    [SerializeField]
+    private Transform pool;
+    [Header("Drag from the project")]
+    [SerializeField]
+    private Material[] materials;
+    [SerializeField]
+    private GameObject objectPrefab;
+
+    private int height;
+    private int color;
+
+    // ===============================================================
+
+
+
+    /// <summary>
+    /// Get the necessary height
+    /// </summary>
+    /// <param name="_height">The height of the spawned object</param>
+    public void GetHeight(int _height)
+    {
+        height = _height;
+    }
+
+    /// <summary>
+    /// Get the necessary color
+    /// </summary>
+    /// <param name="_color">The color of the spawned object</param>
+    public void GetColor(int _color)
+    {
+        color = _color;
+    }
+
+    /// <summary>
+    /// Creating new instance of prefab.
+    /// </summary>
+    public void GetNewInstance()
+    {
+        GameObject objectInstance;
+
+        objectInstance = Instantiate(objectPrefab, spawner.position, Quaternion.identity, pool);
+        
+        // Set the properties of the new instance
+        objectInstance.transform.localScale = new Vector3(height, height, height);
+        objectInstance.GetComponent<MeshRenderer>().material = materials[color];
+    }
+
+}
---
+++ Assets/Prototype.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prototype : MonoBehaviour
+{
+    // ========================== VARIABLES ==========================
+
+    [Header("Objects in the scene")]
+    [SerializeField]
+    private Transform spawner;
+    [SerializeField]
+    private Transform pool;
+    [Header("Drag from the project")]
+    [SerializeField]
+    private Material[] materials;
+    [SerializeField]
+    private GameObject objectPrefab;
+
+    private int height;
+    private int color;
+    private GameObject lastObjectCreated;
+
+    // ===============================================================
+
+    public void Clone()
+    {
+        if(pool.childCount == 0)
+        {
+            print("There are no object in the pool !");
+            return;
+        }
+
+        lastObjectCreated = pool.GetChild(pool.childCount - 1).gameObject;
+
+        height = (int)lastObjectCreated.transform.localScale.x;
+        for(int i = 0; i < materials.Length; i++)
+        {
+            if (materials[i].color == lastObjectCreated.GetComponent<MeshRenderer>().material.color)
+            {
+                color = i;
+            }
+        }
+
+        GameObject objectInstance;
+
+        objectInstance = Instantiate(objectPrefab, spawner.position, Quaternion.identity, pool);
+
+        // Set the properties of the new instance
+        objectInstance.transform.localScale = new Vector3(height, height, height);
+        objectInstance.GetComponent<MeshRenderer>().material = materials[color];
+    }
+}
